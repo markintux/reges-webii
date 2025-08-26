92f5542019df35f608714dea896bc7093e0754f2
Marcos Vinicius <markintux@gmail.com>
2025-08-26 19:49:36 -0300

    :sparkles: feat: protege rotas que necessitam de autenticação

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,18 @@ use App\Http\Controllers\ReminderController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.attempt');
-
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-
 Route::get('/profile', [UserController::class, 'create'])->name('user.create');
 Route::post('/profile', [UserController::class, 'store'])->name('user.store');
 
-Route::resource('reminders', ReminderController::class);
+Route::middleware('auth')
+    ->group(function () {
 
-Route::patch('reminders/{reminder}/done', [ReminderController::class, 'markAsDone'])->name('reminders.done');
-Route::patch('reminders/{reminder}/undone', [ReminderController::class, 'markAsUndone'])->name('reminders.undone');
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('reminders', ReminderController::class);
+        Route::patch('reminders/{reminder}/done', [ReminderController::class, 'markAsDone'])->name('reminders.done');
+        Route::patch('reminders/{reminder}/undone', [ReminderController::class, 'markAsUndone'])->name('reminders.undone');
+        Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+});
